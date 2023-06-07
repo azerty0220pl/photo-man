559d65c1f3cf27ceef222715ad62b4b6bd28b69f
Szymon Kokot <azerty0220pl@gmail.com>
2023-06-07 12:20:35 +0200

    home basic setup

--- src/home.jsx
+++ src/home.jsx
@@ -1,21 +1,35 @@
 import { ButtonGroup, Button, TextField, InputAdornment } from "@mui/material";
-//import { SearchIcon } from "@mui/icons-material";
+import SearchIcon from '@mui/icons-material/Search';
 import logo from "./photoman.png";
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+        main: '#000',
+    }
+  },
+  typography: {
+    fontFamily: 'Courier New'
+  }
+});
 
 const Home = () => {
     return (
         <div>
             <img src={logo} />
             <ButtonGroup>
-                <Button>Search</Button>
-                <Button>My photos</Button>
+                <Button theme={theme}>Search</Button>
+                <Button theme={theme}>My photos</Button>
             </ButtonGroup>
             <TextField
+                theme={theme}
                 id="input-with-icon-textfield"
                 placeholder="Search..."
                 InputProps={{
                     startAdornment: (
                         <InputAdornment position="start">
+                            <SearchIcon theme={theme} />
                         </InputAdornment>
                     ),
                 }}
