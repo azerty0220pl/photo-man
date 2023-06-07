36babbf29e4a83107fa86cf387390400ee0f976a
Szymon Kokot <azerty0220pl@gmail.com>
2023-06-07 14:56:25 +0200

    Home mostly finished

--- src/home.jsx
+++ src/home.jsx
@@ -1,28 +1,27 @@
 import { ButtonGroup, Button, TextField, InputAdornment } from "@mui/material";
 import SearchIcon from '@mui/icons-material/Search';
-import logo from "./photoman.png";
+import logo from "./photoman.svg";
 import { createTheme } from "@mui/material/styles";
+import "./home.css"
 
 const theme = createTheme({
-  palette: {
-    primary: {
-        main: '#000',
+    palette: {
+        primary: {
+            main: '#000',
+        }
+    },
+    typography: {
+        fontFamily: 'Courier New'
     }
-  },
-  typography: {
-    fontFamily: 'Courier New'
-  }
 });
 
-const Home = () => {
+const Home = ({ current }) => {
     return (
-        <div>
-            <img src={logo} />
-            <ButtonGroup>
-                <Button theme={theme}>Search</Button>
-                <Button theme={theme}>My photos</Button>
-            </ButtonGroup>
+        <div className="home">
+            <img className="logo" src={logo} />
+            <Button theme={theme} variant="outlined" sx={{ boxShadow: 3 }} href={current === '0' ? '/favourites/' : '/'}>{current === '0' ? 'Go to favourites' : 'Search new photos'}</Button>
             <TextField
+                className="input"
                 theme={theme}
                 id="input-with-icon-textfield"
                 placeholder="Search..."
@@ -34,6 +33,14 @@ const Home = () => {
                     ),
                 }}
                 variant="standard"
+                sx={{
+                    "& .MuiInput-underline:after": {
+                        borderBottomColor: "#000"
+                    },
+                    "& .MuiInput-input": {
+                        fontFamily: 'Courier New'
+                    }
+                }}
             />
         </div>
     );
