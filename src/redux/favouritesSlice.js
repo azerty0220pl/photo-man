f80d44f74925df62d1aabac7f2ba3745319770e2
Szymon Kokot <azerty0220pl@gmail.com>
2023-06-07 19:34:35 +0200

    Slices fixed

--- src/redux/favouritesSlice.js
+++ src/redux/favouritesSlice.js
@@ -1,13 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const defaultState = {
-    photos: JSON.parse(localStorage.getItem("photos")) == null ? [] : JSON.parse(localStorage.getItem("photos")).values
+    photos: JSON.parse(localStorage.getItem("photos")) == null ? [] : JSON.parse(localStorage.getItem("photos")).values,
+    filter: ""
 };
 
 const favouritesSlice = createSlice({
     name: "favourites",
     initialState: defaultState,
     reducers: {
+        filterQuery: (state, action) => {
+            state.filter = action.payload;
+            return state;
+        },
         description: (state, action) => {
             state.photos.find(e => e.id === action.payload.id).description = action.payload.description;
             return state;
@@ -19,6 +24,6 @@ const favouritesSlice = createSlice({
     }
 });
 
-export const { description, remove } = favouritesSlice.actions;
+export const { filterQuery, description, remove } = favouritesSlice.actions;
 
 export default favouritesSlice.reducer;
--- src/redux/searchSlice.js
+++ src/redux/searchSlice.js
@@ -1,4 +1,5 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, applyMiddleware } from "@reduxjs/toolkit";
+import thunk from 'redux-thunk';
 
 const defaultState = {
     photos: [],
@@ -9,36 +10,12 @@ const searchSlice = createSlice({
     name: "search",
     initialState: defaultState,
     reducers: {
-        search: async (state, action) => {
-            let data;
-
-            if (action.payload === "")
-                data = await fetch("https://api.unsplash.com/photos/random?count=10");
-            else
-                data = await fetch("https://api.unsplash.com/search/photos?query=" + action.payload);
-
-            let local = JSON.parse(localStorage.getItem("photos")) == null ? [] : JSON.parse(localStorage.getItem("photos")).values;
-
-            data = await data.map(async x => {
-                let aux = await x.json();
-                let res = {
-                    id: aux.id,
-                    description: aux.description,
-                    width: aux.width,
-                    height: aux.height,
-                    likes: aux.likes,
-                    urls: {
-                        full: aux.urls.full,
-                        thumb: aux.urls.thumb
-                    },
-                    saved: local.filter(e => e.id === aux.id).length > 0
-                }
-
-                return res;
-            });
-
-            state.search = action.payload;
-            state.photos = data;
+        searchQuery: (state, action) => {
+            state.searched = action.payload;
+            return state;
+        },
+        loadPhotos: (state, action) => {
+            state.photos = action.payload;
             return state;
         },
         save: (state, action) => {
@@ -47,7 +24,7 @@ const searchSlice = createSlice({
 
             let local = JSON.parse(localStorage.getItem("photos")) == null ? [] : JSON.parse(localStorage.getItem("photos")).values;
 
-            if(photo.saved)
+            if (photo.saved)
                 local.push(photo);
             else
                 local.slice(local.findIndex(e => e.id === action.payload), 1);
@@ -57,8 +34,53 @@ const searchSlice = createSlice({
             return state;
         }
     }
-});
+}, applyMiddleware(thunk));
 
-export const { search, save } = searchSlice.actions;
+export const { searchQuery, loadPhotos, save } = searchSlice.actions;
+
+export const searchPhotos = () => {
+    return async (dispatch, getState) => {
+        let state = getState();
+
+        let data;
+
+        if (state.search.searched === "")
+            await fetch("https://api.unsplash.com/photos/random?count=10", { method: 'GET', headers: { 'Authorization': "Client-ID " + import.meta.env.VITE_UNSPLASH_KEY } })
+                .then(async d => {
+                    await d.json().then(x => {
+                        data = x;
+                    });
+                });
+        else
+            await fetch("https://api.unsplash.com/search/photos?query=" + state.search.searched, { method: 'GET', headers: { 'Authorization': "Client-ID " + import.meta.env.VITE_UNSPLASH_KEY } })
+                .then(async d => {
+                    await d.json().then(x => {
+                        data = x;
+                    });
+                });
+
+        let local = JSON.parse(localStorage.getItem("photos")) == null ? [] : JSON.parse(localStorage.getItem("photos")).values;
+
+        console.log(data);
+        let photos = data.results.map( x => {
+            let res = {
+                id: x.id,
+                description: x.description,
+                width: x.width,
+                height: x.height,
+                likes: x.likes,
+                urls: {
+                    full: x.urls.full,
+                    thumb: x.urls.thumb
+                },
+                saved: local.filter(e => e.id === x.id).length > 0
+            }
+
+            return res;
+        });
+
+        dispatch(searchSlice.actions.loadPhotos(photos));
+    }
+}
 
 export default searchSlice.reducer;
