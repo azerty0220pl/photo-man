549454e87ae24474c3ca575fe31771bd34205280
Szymon Kokot <azerty0220pl@gmail.com>
2023-06-08 12:31:13 +0200

    Gallery fix

--- src/favourite.jsx
+++ src/favourite.jsx
@@ -1,20 +1,22 @@
 import Home from "./home";
 import { useSelector } from 'react-redux';
 import Photo from "./photo";
+import './page.css';
 
 const Favourite = () => {
-    filter = useSelector((state) => { state.favourites.filter });
-
+    let filter = useSelector((state) => state.favourites.filter);
+    let photos = useSelector((state) => state.favourites.photos);
     return (
         <div>
             <Home current="1" />
             <div className="gallery">
                 {
-                    getPhotos.map((x, i) => {
-                        if (filter.length > 0 && !x.description.includes(filter))
-                            return <></>
-                        return <Photo photo={x} key={i} />
-                    })
+                    photos == null ? <></> :
+                        photos.map((x, i) => {
+                            if (filter.length > 0 && !x.description.includes(filter))
+                                return <></>
+                            return <Photo photo={x} key={i} />
+                        })
                 }
             </div>
         </div>
