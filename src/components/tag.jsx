7b5d868c22ebe5fa85c9b0a717a9871f69756177
Szymon Kokot <azerty0220pl@gmail.com>
2023-06-14 09:10:06 +0200

    tag element created

---
+++ src/components/tag.jsx
@@ -0,0 +1,11 @@
+import { Chip } from "@mui/material";
+import { useState } from "react";
+
+const Tag = ({ name,  }) => {
+
+    return <Chip label={name} variant={state} onClick={() => {
+        
+    }} />
+}
+
+export default Tag;
