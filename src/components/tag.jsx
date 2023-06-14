52f1eb347f0fd9fff5233468d68410d2fd8ff8c7
Szymon Kokot <azerty0220pl@gmail.com>
2023-06-14 09:10:21 +0200

    tag change on click

--- src/components/tag.jsx
+++ src/components/tag.jsx
@@ -2,9 +2,17 @@ import { Chip } from "@mui/material";
 import { useState } from "react";
 
 const Tag = ({ name,  }) => {
+    let [state, setState] = useState("outlined");
 
     return <Chip label={name} variant={state} onClick={() => {
         
+        console.log(state)
+
+        if (state === 'outlined') {
+            setState("filled");
+        } else {
+            setState('outlined');
+        }
     }} />
 }
 
